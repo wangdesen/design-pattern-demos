930d87940950a8e8cb970f18f62475d4947bd560
wangdesen <desen000@gmail.com>
2016-06-27 17:19:00 +0800

    templatemethod pattern add hook's annotation

--- src/main/java/com/wangdesen/dp/templatemethod/barista/CaffeineBeverageWithHook.java
+++ src/main/java/com/wangdesen/dp/templatemethod/barista/CaffeineBeverageWithHook.java
@@ -8,6 +8,9 @@ package com.wangdesen.dp.templatemethod.barista;
  * */
 public abstract class CaffeineBeverageWithHook {
  
+	/**
+	 * 准备步骤--使用了钩子
+	 * */
 	public void prepareRecipe() {
 		boilWater();
 		brew();
@@ -17,19 +20,29 @@ public abstract class CaffeineBeverageWithHook {
 		}
 	}
  
+	//冲入
 	abstract void brew();
  
+	//添加调料
 	abstract void addCondiments();
  
+	//开水
 	void boilWater() {
 		System.out.println("Boiling water");
 	}
  
+	//倒入杯子
 	void pourInCup() {
 		System.out.println("Pouring into cup");
 	}
  
-	//客户是否要加调料
+	/**
+	 * 客户是否要加调料
+	 * 
+	 * 这是一个钩子，子类可以选择实现钩子，也可以不做
+	 * 
+	 * 做钩子的一般目的，就是为了在子类中灵活处理
+	 * */
 	boolean customerWantsCondiments() {
 		return true;
 	}
