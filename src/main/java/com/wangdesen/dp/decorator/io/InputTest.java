13834a6a61dc712808545c69f27ca08832083fb5
wangdesen <desen000@gmail.com>
2016-11-29 14:07:04 +0800

    decorator pattern update io module

--- src/main/java/com/wangdesen/dp/decorator/io/InputTest.java
+++ src/main/java/com/wangdesen/dp/decorator/io/InputTest.java
@@ -2,20 +2,20 @@ package com.wangdesen.dp.decorator.io;
 
 import java.io.*;
 
+/**
+ * 采用装饰者模式的小写转换方法测试类
+ * 
+ * @author wangdesen
+ */
 public class InputTest {
+	
 	public static void main(String[] args) throws IOException {
 		int c;
-
 		try {
-			InputStream in = 
-				new LowerCaseInputStream(
-					new BufferedInputStream(
-						new FileInputStream("test.txt")));
-
-			while((c = in.read()) >= 0) {
-				System.out.print((char)c);
+			InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("C:/test.txt")));
+			while ((c = in.read()) >= 0) {
+				System.out.print((char) c);
 			}
-
 			in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/main/java/com/wangdesen/dp/decorator/io/LowerCaseInputStream.java
+++ src/main/java/com/wangdesen/dp/decorator/io/LowerCaseInputStream.java
@@ -3,8 +3,6 @@ package com.wangdesen.dp.decorator.io;
 import java.io.*;
 
 /**
- * java io decorator
- * 
  * 采用了装饰者模式，对于字节流的处理，不是很清楚
  * */
 public class LowerCaseInputStream extends FilterInputStream {
@@ -13,13 +11,17 @@ public class LowerCaseInputStream extends FilterInputStream {
 		super(in);
 	}
  
-	//针对字节的read方法
+	/**
+	 * 针对字节的read方法
+	 */
 	public int read() throws IOException {
 		int c = super.read();
 		return (c == -1 ? c : Character.toLowerCase((char)c));
 	}
 		
-	//针对字节数组的read方法
+	/**
+	 * 针对字节数组的read方法
+	 */
 	public int read(byte[] b, int offset, int len) throws IOException {
 		int result = super.read(b, offset, len);
 		for (int i = offset; i < offset+result; i++) {
